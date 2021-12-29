de1238a057bcc60a979d9130b3c607bc0c29bc50
python <python290821@gmail.com>
2021-12-29 20:27:47 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,15 +1,29 @@
 
-# try - else
+# input salary
+# check if salary 5000, 15,000
+
+# custom exception
+class SalaryNotInRangeError(Exception):
+    def __init__(self, salary, message="Salary is not in (5000, 15000) range"):
+        self.message = message
+        self.salary = salary
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'SalaryNotInRangeError: {self.message}'
+
+def get_salary():
+    salary = int(input("Enter salary: "))
+    if not 5000 < salary < 15000:
+        raise SalaryNotInRangeError(salary)
+    return salary
+
 try:
-    int1_safe = int(input('Enter number (i dont trust you): '))
-except ValueError as e:
+    salary = get_salary()
+except SalaryNotInRangeError as e:
     print(e)
-else:
-    # out of try scope, but still execute finally after me ...............
-    int2_unsafe = int(input('Enter number (i trust you): '))
-    ################################
-finally: # will always be executed
-    print('thank you for your cooperation')
+    print(f'you entered {e.salary} which is illegal')
+
 
 
 
